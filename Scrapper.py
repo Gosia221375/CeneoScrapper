8969105ae76995bc32e2b70f2778b920db969eec
Gosia221375 <s221375@student.uek.krakow.pl>
2022-04-05 13:03:49 +0200

    2022-04-05 ML

--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.ceneo.pl/45863470#tab-reviews"
+url = "https://www.ceneo.pl/45863470#tab=reviews"
 
 response = requests.get(url)
 
@@ -10,8 +10,27 @@ print(page_dom.prettify())
 
 opinions = page_dom.select("div.js_product-review")
 print(type(opinions))
-
-
-
 opinion = opinions.pop()
+
+opinion_id = opinion["data-entry-id"]
+author = opinion.select_one("span.user-post_author-name").text.strip()
+rcmd = opinion.select_one("span.user-post_author-recommendation > em").text.strip()
+score = opinion.select_one("span.user-post_score-count").text.strip()
+content = opinion.select_one("div.user-post_text").text.strip()
+posted_on = opinion.select_one("span.user-post_published > time:nth-child(1)")["datetime"]
+bought_on = opinion.select_one("span.user-post_published > time:nth-child(2)")["datetime"]
+useful_for = opinion.select_one("button.vote-yes > span").text.strip()
+useless_for = opinion.select_one("button.vote-no > span").text.strip()
+pros = opinion.select("div.review-feature_title--positives ~ div.review-feature_item")
+pros = [item.text.strip() for item in pros]
+
+print(type(pros))
+print(pros)
+
+
+
+print(type(author))
+print(author)
+
+
 print(opinion.prettify())
